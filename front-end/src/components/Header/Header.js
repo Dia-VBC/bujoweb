f324fda536db52f0787d7da8ef451c0b86cd67d1
Nadiajda Cavalcanti <nadicavalcanti@gmail.com>
2018-12-18 23:51:30 -0200

    Componente Header

---
+++ front-end/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.css';
+
+export default function Header() {
+  return (
+    <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
+      <a href="key.js"><h1 className="label">BuJo</h1></a>
+      <ul> 
+      	<li><a href="day.js"><h3 className="label">Dia</h3></a></li>
+      	<li><a href="week.js"><h3 className="label">Semana</h3></a></li>
+      	<li><a href="month.js"><h3 className="label">Mês</h3></a></li>
+      </ul>
+    </div>
+  )
+}
