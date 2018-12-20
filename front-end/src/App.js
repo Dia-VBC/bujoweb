bce15bdade86a3d74daa38bdac7d5af533e09d79
Nadiajda Cavalcanti <nadicavalcanti@gmail.com>
2018-12-19 23:13:46 -0200

    Adaptação do layout e finalização de Key, agora no próprio App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,12 +4,35 @@ import './App.css';
 
 import Header from './components/Header';
 import Body from './components/Body';
+import StickyNote from './components/StickyNote';
 class App extends Component {
   render() {
     return (
       <div>
       <Header />
       <Body />
+      <div className="content">
+      	<h1>Key</h1>
+      	<ul>
+      	<li>
+      	<StickyNote />
+      		<h3>Tarefas</h3>
+      		<p>o <br/>Não finalizada</p>
+      		<p className="linethrough">Finalizada</p>
+      	</li>
+      	<li>
+      	<StickyNote />
+      		<h3>Notas</h3>
+      		<p>- <br/>Anotação</p>
+      	</li>
+      	<li>
+      	<StickyNote />
+      		<h3>Eventos</h3>
+      		<p># <br/>Não ocorreu ainda</p>
+      		<p className="linethrough">Ocorreu</p>
+      	</li>
+      	</ul>
+      </div>
       </div>
     );
   }
--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './index.css';
 export default function Header() {
   return (
     <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
-      <a href="../Key.js"><h1 className="label">BuJo</h1></a>
+      <a href="../App.js"><h1 className="label">BuJo</h1></a>
       <ul> 
       	<li><a href="day.js"><h3 className="label">Dia</h3></a></li>
       	<li><a href="week.js"><h3 className="label">Semana</h3></a></li>
