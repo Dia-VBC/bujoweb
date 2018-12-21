ef830f8ea563e60d00d4c9c4dd0719dfc1c31c9a
Nadiajda Cavalcanti <nadicavalcanti@gmail.com>
2018-12-20 23:08:21 -0200

    Formulário NewTask

--- front-end/src/NewTask.js
+++ front-end/src/NewTask.js
@@ -12,16 +12,22 @@ class NewTask extends Component {
       <Header />
       <Body />
       <div className="content">
-      	<h1>Nova tarefa</h1>
-      	<ul>
-      	<li>
-      		<div className="Conteudo">
-      		<h2></h2>
-      		
-      		</div>
-      	</li>
+      	<h1>Novo Bullet</h1>
 
-      	</ul>
+            <form>
+                  <label>Tipo:</label>
+                  <select name="type">
+                        <option value="1">Tarefa</option>
+                        <option value="2">Anotação</option>
+                        <option value="3">Evento</option>
+                  </select>
+                  <br/>
+                  <label>Descrição:</label>
+                  <input type="textbox" className="textbox" name="decrição" placeholder="Descrição"/>
+                  <br/>
+                  <button type="button" onclick="" className="salvarTask">Salvar</button>
+            </form>
+
       </div>
       </div>
  );
